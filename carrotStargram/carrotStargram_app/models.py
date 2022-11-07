07d6d106a7478757de0460f950061389fb425f4b
jhwan-g <doveuyhtgfedr@gmail.com>
2022-11-07 12:18:37 +0900

    model 수정 - friendtable 만듦 - account, post에 imagefield추가 (미래에 만들 경우 대비) - post 좋아요를 위한 manytomanyfield 추가

--- carrotStargram/carrotStargram_app/models.py
+++ carrotStargram/carrotStargram_app/models.py
@@ -1,23 +1,20 @@
 from django.db import models
 
 # Create your models here.
-
-
 class Account(models.Model):
     name = models.charField(max_length=50)
     description = models.charField(max_length=100)
-
-
+    account_img = models.ImageField(upload_to='profiles_imgs')
 
 class FriendTable(models.Model):
-    people1 = models.ManyToManyField(Account)
-
-
-
-
+    people1 = models.ForeignKey(Account)
+    people2 = models.ForeignKey(Account)
 
 class Post(models.Model):
     name = models.CharField(max_length=50)
     description = models.charField(max_length=100)
     link = models.URLField(max_length=200)
     uploader = models.ForeignKey(Account, on_delete=models.CASCADE)
+    like = models.ManyToManyField(Account)
+    post_img = models.ImageField(upload_to='post_imgs')
+
