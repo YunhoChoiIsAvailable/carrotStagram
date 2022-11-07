f51dd82fdc3c752e362729c48bfd954ce8918f3c
YunhoChoi <choiyunho0503@gmail.com>
2022-11-07 10:29:32 +0900

    admin and app, models no friends yet

--- carrotStargram/carrotStargram_app/models.py
+++ carrotStargram/carrotStargram_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Account(models.Model):
+    name = models.charField(max_length=50)
+    description = models.charField(max_length=100)
+
+
+
+class FriendTable(models.Model):
+    people1 = models.ManyToManyField(Account)
+
+
+
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.charField(max_length=100)
+    link = models.URLField(max_length=200)
+    uploader = models.ForeignKey(Account, on_delete=models.CASCADE)
